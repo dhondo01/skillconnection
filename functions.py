c5cd4b7238edf1b01a7017f6651796f07ca69a59
whoisduley <dulelu01@luther.edu>
2014-12-14 14:01:15 -0600

    cleaning'

--- functions.py
+++
@@ -1,7 +0,0 @@
-# Takes input from forms to create a student object
-def studentCreate():
-
-# Compares a student object's skills with job skills. Adds to connection table and returns the connections
-def studentSearch(student_id):
-
-
